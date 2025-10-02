85219e82be0aeeacaf55f3ea4b52583fd6f22258
MisterE1234 <MisterE1234@outlook.com>
2025-10-02 15:08:40 -0700

    2nd Commit

--- 210-Lab-17.cpp
+++ 210-Lab-17.cpp
@@ -1,2 +1,135 @@
 //COMSC-210|Lab-17|Ethan Aylard
 //IDE used: Visual Studio Code
+//This program demonstrates linked lists with functions.
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 7;  
+
+struct Node {
+    float value;
+    Node *next;
+};
+
+int menu();
+void output(Node *);
+void addFront(Node *, int);
+void addEnd(Node *, int);
+void deleteNode(Node *, int);
+void insertNode(Node *, int, int);
+void deleteList(Node *);
+
+
+int main() {
+    Node *head = nullptr;
+    int count = 0;
+    int choice = -1;
+
+    // create a linked list of size SIZE with random numbers 0-99
+    for (int i = 0; i < SIZE; i++) {
+        int tmp_val = rand() % 100;
+        Node *newVal = new Node;
+        
+        // adds node at head
+        if (!head) { // if this is the first node, it's the new head
+            head = newVal;
+            newVal->next = nullptr;
+            newVal->value = tmp_val;
+        }
+        else { // its a second or subsequent node; place at the head
+            newVal->next = head;
+            newVal->value = tmp_val;
+            head = newVal;
+        }
+    }
+    output(head);
+    
+    while(choice != 0){
+
+        choice = menu();
+        switch(choice){
+            case 1: {
+                int val;
+                cout << "Enter a value to add to the front of the list: ";
+                cin >> val;
+                addFront(head, val);
+                output(head);
+                break;
+            }
+            case 2: {
+                int val;
+                cout << "Enter a value to add to the end of the list: ";
+                cin >> val;
+                addEnd(head, val);
+                output(head);
+                break;
+            }
+            case 3: {
+                int val;
+                cout << "Enter a value to delete from the list: ";
+                cin >> val;
+                deleteNode(head, val);
+                output(head);
+                break;
+            }
+            case 4: {
+                int afterVal, newVal;
+                cout << "Enter a value to insert after: ";
+                cin >> afterVal;
+                cout << "Enter a new value to insert: ";
+                cin >> newVal;
+                insertNode(head, afterVal, newVal);
+                output(head);
+                break;
+            }
+            case 5: {
+                deleteList(head);
+                head = nullptr; // reset head to nullptr after deletion
+                output(head);
+                break;
+            }
+
+            case 7: {
+                // option to display the list 
+                output(head);
+                break;
+            }
+            case 0:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+    
+    
+    Node * current = head;
+    current = head;
+    while (current) {
+        head = current->next;
+        delete current;
+        current = head;
+    }
+    head = nullptr;
+    output(head);
+
+    delete head;
+
+    return 0;
+}
+
+void output(Node * hd) {
+    if (!hd) {
+        cout << "Empty list.\n";
+        return;
+    }
+    int count = 1;
+    Node * current = hd;
+    while (current) {
+        cout << "[" << count++ << "] " << current->value << endl;
+        current = current->next;
+    }
+    cout << endl;
+}
+
